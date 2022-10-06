a926a6d23ec289ccf82c559371d014f881cbd24c
Lucas Galdino <lucasgaldino7@gmail.com>
2022-10-06 08:21:56 -0300

    371_logica_auth_03

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -3,16 +3,24 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static final Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+
+  @override
   ChatUser? get currentUser {
-    return null;
+    return _currentUser;
   }
 
+  @override
   Stream<ChatUser?> get userChanges {}
 
+  @override
   Future<void> signup(
       String nome, String email, String password, File image) async {}
 
+  @override
   Future<void> login(String email, String password) async {}
 
+  @override
   Future<void> logout() async {}
 }
