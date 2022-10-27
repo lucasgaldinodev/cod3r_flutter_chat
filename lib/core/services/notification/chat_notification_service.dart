5fc725cdaf1a1a38028d1f48ca61ef051b8c2508
Lucas Galdino <lucasgaldino7@gmail.com>
2022-10-27 14:59:50 -0300

    414_push_notification_05

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -25,9 +25,9 @@ class ChatNotificationService with ChangeNotifier {
 
   // Push Notification
   Future<void> init() async {
+    await _configureTerminated();
     await _configureForeground();
     await _configureBackground();
-    await _configureTerminated();
   }
 
   Future<bool> get _isAuthorized async {
